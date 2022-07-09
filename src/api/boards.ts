d213b6f6ca2fe9795a784b64b16ac99559e6bdd6
Jonathan Hernández <jonathan.hernandez@gmail.com>
2022-07-09 15:11:12 -0600

    Get board by Id and add default columns

--- src/api/boards.ts
+++ src/api/boards.ts
@@ -1,20 +1,64 @@
 import { prisma } from '../db'
 import { Request, Response } from 'express'
+import { ColumnData } from '../types'
 
 // Get All Boards
 export const getAllBoards = async (req: Request, res: Response) => {
   const boards = await prisma.board.findMany()
-  res.json({data: boards})
+  res.json({ data: boards })
+}
+
+// Get Board by id
+export const getBoard = async (req: Request, res: Response) => {
+  const id = parseInt(req.params['id'])
+  const board = await prisma.board.findUnique({
+    where: { id },
+    include: { 
+      columns: { 
+        include: { 
+          tasks: true 
+        } 
+      } 
+    }
+  })
+  res.json({ data: board })
 }
 
 // Create Board
 export const createBoard = async (req: Request, res: Response) => {
   const { title, description } = req.body
+
   const result = await prisma.board.create({
     data: {
       title,
       description
     }
   })
-  res.json(result)
+  // Default columns
+  let defaultColumns: ColumnData[] = [
+    {
+      title: 'To Do',
+      position: 0,
+      boardId: result.id
+    },
+    {
+      title: 'In Process',
+      position: 1,
+      boardId: result.id
+    },
+    {
+      title: 'Done',
+      position: 2,
+      boardId: result.id
+    }
+    
+  ]
+  await Promise.all(
+    defaultColumns.map(async (col) => {
+      await prisma.column.create({
+        data: col,
+      })
+    })
+  )
+  res.json({ data: result })
 }
