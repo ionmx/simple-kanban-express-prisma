f84f45567cfd29fb474684d85f1d78a0af8df9c9
Jonathan Hernández <jonathan.hernandez@gmail.com>
2022-07-16 14:24:14 -0600

    task data type

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,13 @@ import { Prisma } from '@prisma/client';
 
 const columnData = Prisma.validator<Prisma.ColumnArgs>()({
   select: { title: true, position: true, boardId: true }
-});
+})
 
-export type ColumnData = Prisma.ColumnGetPayload<typeof columnData>;
+export type ColumnData = Prisma.ColumnGetPayload<typeof columnData>
+
+const taskData = Prisma.validator<Prisma.TaskArgs>()({
+  select: { description: true, position: true, columnId: true }
+})
+
+export type TaskData = Prisma.TaskGetPayload<typeof columnData>
+
