15b003693c2679cad3ab99c8d4b4e3b4adef7f65
Jonathan Hernández <jonathan.hernandez@gmail.com>
2022-07-16 14:24:57 -0600

    moveColumn end point

--- src/api/columns.ts
+++ src/api/columns.ts
@@ -1,13 +1,36 @@
 import { prisma } from '../db'
-import { Prisma } from '@prisma/client'
 import { Request, Response } from 'express'
 
 // Create Column
 export const createColumn = async (req: Request, res: Response) => {
-  const id = parseInt(req.params['id'])
+  const boardId = parseInt(req.params['board_id'])
   const { title } = req.body
   const result = await prisma.column.create({
-    data: { title, boardId: id }
+    data: { title, boardId: boardId }
   })
   res.json({ data: result })
+}
+
+// Move Column
+export const moveColumn = async (req: Request, res: Response) => {
+  const boardId = parseInt(req.params['board_id'])
+  const id = parseInt(req.params['id'])
+  const { position } = req.body
+  const column = await prisma.column.findUnique({
+    where: { id }
+  })
+
+  if (column) {
+    // Update source positions
+    await prisma.column.updateMany({
+      where: { boardId: boardId, position: { gt: column.position } },
+      data: { position: { decrement: 1 } }
+    })
+    // Update target positions
+    await prisma.column.updateMany({
+      where: { boardId: boardId, position: { gte: position } },
+      data: { position: { increment: 1 } }
+    })
+  }
+  res.json({ data: column })
 }
