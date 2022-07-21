675dec2eda7b1f2b19ebb4255a968e1071cbc1fd
Jonathan Hernández <jonathan.hernandez@gmail.com>
2022-07-21 14:40:41 -0600

    Move task

--- src/api/tasks.ts
+++ src/api/tasks.ts
@@ -16,12 +16,52 @@ export const createTask = async (req: Request, res: Response) => {
     }
   })
 
-  if (taskAggregate._max.position) {
+  if (taskAggregate._max.position !== null) {
+    console.log(taskAggregate._max.position)
     position = taskAggregate._max.position + 1
+  } else {
+    position = 0
   }
 
   const result = await prisma.task.create({
-    data: { description, columnId: columnId, position: position }
+    data: { description, columnId: columnId, position: position}
   })
   res.json({ data: result })
 }
+
+// Move Task
+export const moveTask = async (req: Request, res: Response) => {
+  const boardId = parseInt(req.params['board_id'])
+  const { task, destination, position } = req.body
+
+  const the_task = await prisma.task.findUnique({
+    where: { id: task }
+  })
+
+  if (the_task) {
+    // Update source positions
+    await prisma.task.updateMany({
+      where: { columnId: the_task.columnId, position: { gt: the_task.position } },
+      data: { position: { decrement: 1 } }
+    })
+    // Update target positions
+    await prisma.task.updateMany({
+      where: {columnId: destination, position: { gte: position } },
+      data: { position: { increment: 1 } }
+    })
+
+    const updatedTask = await prisma.task.update({
+      where: {
+        id: task,
+      },
+      data: {
+        columnId: destination,
+        position: position
+      }
+    })
+    res.json({ data: updatedTask })
+  }
+  res.json({ data: null })
+  
+}
+
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import { getAllBoards, getBoard, createBoard } from './api/boards'
 import { createColumn, moveColumn } from './api/columns'
-import { createTask } from './api/tasks'
+import { createTask, moveTask } from './api/tasks'
 
 const app = express()
 
@@ -20,6 +20,7 @@ app.post(`/api/v1/boards/:board_id/move-column`, moveColumn)
 
 // Task routes
 app.post(`/api/v1/boards/:board_id/columns/:column_id/tasks`, createTask)
+app.post(`/api/v1/boards/:board_id/move-task`, moveTask)
 
 const server = app.listen(4000, () =>
   console.log(`Server ready at: http://localhost:4000`),
